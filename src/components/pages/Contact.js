04cbac31418ea0ddfdf25a484bea782964447ecf
hxn8439 <52980606+hxn8439@users.noreply.github.com>
2020-10-05 16:55:08 -0500

    Installed MDB modules to Contact form
    Installed MDB moduels to contact us form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,45 +1,35 @@
 import React from 'react';
 import '../../App.css';
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBIcon, MDBInput } from 'mdbreact';
 
-
-
-function Contact() {
-
-
-  return (
-
-    <div className='hero-container'>  
-  <form>
-    <h1>Contact Us!</h1>
-  <label>
-    Your Name:
-    <input type="text" name="name" />
-  </label>
-  </form>
-
-  <form>
-  <label>
-    Email Address:
-    <input type="text" name="address" />
-  </label>
-  </form>
-
-  <form>
-  <label>
-    
-    <textarea placeholder="Email body:" rows="15"></textarea>
-  </label>
-  </form>
-
-  <form>
-  <input type="submit" value="Submit" />  
-  </form>
-  
-  
-  </div>
-  );
-
-
-}
+const Contact = () => {
+return (
+<div className='hero-container'>   
+<MDBContainer>
+  <MDBRow>
+    <MDBCol md="6">
+      <form>
+        <p className="h5 text-center mb-4">Write to us</p>
+        <div className="grey-text">
+          <MDBInput label="Your name" icon="user" group type="text" validate error="wrong"
+            success="right" />
+          <MDBInput label="Your email" icon="envelope" group type="email" validate error="wrong"
+            success="right" />
+          <MDBInput label="Subject" icon="tag" group type="text" validate error="wrong" success="right" />
+          <MDBInput type="textarea" rows="2" label="Your message" icon="pencil-alt" />
+        </div>
+        <div className="text-center">
+          <MDBBtn outline color="secondary">
+            Send
+            <MDBIcon far icon="paper-plane" className="ml-1" />
+          </MDBBtn>
+        </div>
+      </form>
+    </MDBCol>
+  </MDBRow>
+</MDBContainer>
+</div>
+);
+};
 
 export default Contact;
