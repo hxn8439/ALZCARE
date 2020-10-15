b58bdb471b35cee8e685298533a84ec5be0b37c0
robbiefrazier <54490358+robbiefrazier@users.noreply.github.com>
2020-10-15 17:50:56 -0500

    Added map to Community Resources page

--- src/components/pages/resources.js
+++ src/components/pages/resources.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import '../../App.css';
+import React from 'react'
+import '../../App.css'
+import MapSection from '../Map'
 
-
+const location = {
+  address: '211 S Cooper St, Arlington, TX',
+  lat: 32.734675,
+  lng: -97.114280,
+} // location of Dr. Lee's office - UTA School of Social Work
 
 function resources() {
   return (
-    <div className='hero-container'>
-
+    <div className="Map">
+      <MapSection location={location} zoomLevel={15} />
     </div>
   );
 }
