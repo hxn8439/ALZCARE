2cf50e85594aeecd734bbcdc44c7b889c66fcb76
Johnny Pham <jpham009@gmail.com>
2020-11-13 14:36:45 -0600

    Alternate Google Maps solution

--- src/components/pages/resources.js
+++ src/components/pages/resources.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '../../App.css'
 import MapSection from '../Map'
 import ResourceTableSection from '../ResourceTableSection'
+
 //importing all dependiencies, libraries, and subsite js pages by category- Hamilton Nguyen 10/19/2020
 
 // This constant location serves as a global position values meta data to be placed on the map. - Hamilton Nguyen 10/19/2020
@@ -13,11 +14,19 @@ const location = {
 
 function resources() {
   return (
-    <div className="Map">
-      <MapSection location={location} zoomLevel={15} />
-      <ResourceTableSection />
-    </div>
-  );
-} // displays a map on the Community Resources page - Robert F. 10/19/2020
+    //Alternate map - Johnny P. 11/13/2020
+    <div>
+    <iframe src="https://www.google.com/maps/d/embed?mid=1benrcNvPLgrYBM4BvHkpL0NzbDumoS5k&hl=en" width="800" height="600"></iframe>
+    <ResourceTableSection />
+    </div>);
+  }
+
+  // Original map
+  //   <div className="Map">
+  //     <MapSection location={location} zoomLevel={15} />
+  //     <ResourceTableSection />
+  //   </div>
+  // );
+  // displays a map on the Community Resources page - Robert F. 10/19/2020
 
 export default resources;
